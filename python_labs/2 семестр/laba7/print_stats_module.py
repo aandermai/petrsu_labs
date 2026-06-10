1786893d054cc0cc0113c69d17714eb07ba23a48
andermai <skimez76@gmail.com>
2026-06-10 12:24:38 +0300

    edit python_labs

--- python_labs/2 семестр/laba7/print_stats_module.py
+++
@@ -1,8 +0,0 @@
-def print_stats(stats, n):
-    # Если пользователь хочет вывести все сегменты
-    if n == -1:
-        for i in range(len(stats)):
-            print(f"Сегмент {i+1}:\n{stats[i]}")
-    # Если пользователь хочет вывести определенный сегмент
-    else:
-        print(f"\n{stats[n]}")
